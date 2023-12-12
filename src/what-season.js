1e34bfd0f3a72255012dea4ad27b3dbf62fa0d14
Demetrius81 <shizimaniak@gmail.com>
2023-12-12 22:46:00 +0300

    feat: chainMaker is allmost done

--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,47 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+    if (!date) {
+        return "Unable to determine the time of year!";
+    }
+
+    if (typeof date === "object" && date.constructor.name === "Date") {
+        let month = null;
+
+        try {
+            month = date.getUTCMonth();
+        } catch {
+            throw new Error("Invalid date!");
+        }
+
+        switch (true) {
+            case month > 1 && month <= 4:
+                return "spring";
+            case month > 4 && month <= 7:
+                return "summer";
+            case month > 7 && month <= 10:
+                return "autumn";
+            case month === 11 || (month >= 0 && month < 2):
+                return "winter";
+            default:
+                throw new Error("Invalid date!");
+        }
+    } else {
+        throw new Error("Invalid date!");
+    }
 }
 
 module.exports = {
-  getSeason
+    getSeason,
 };
