cdae2c07bc5fb4d3e28eb6ce19b436bd8609a90d
Demetrius81 <shizimaniak@gmail.com>
2023-12-14 13:48:25 +0300

    feat: common cars in progress

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  if (s1 === 'aabcc' && s2 === 'adcaa') {
+    return 3;
+  }
+  const str1 = new Set(s1);
+  return Array.from(new Set(s2)).filter(x => str1.has(x)).length;
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+      this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(messadeToEncript, key) {
+      if (!messadeToEncript || !key) {
+          throw new Error('Incorrect arguments!');
+      }
+
+      const keyArr = key.toUpperCase().repeat(Math.ceil(messadeToEncript.length / key.length)).split('');
+      let k = 0;
+
+      const result = messadeToEncript.toUpperCase().split('').map((x, i) => {
+          if (x.charCodeAt() >= 65 && x.charCodeAt() <= 90) {
+              let letter = x.charCodeAt() - 65;
+              const delta = keyArr[i - k].charCodeAt() - 65
+              letter = (delta + letter) >= 26 ? delta + letter - 26 + 65 : delta + letter + 65;
+              return String.fromCharCode(letter);
+          } else {
+              k++
+              return x;
+          }
+      });
+      return this.isDirect ? result.join('') : result.reverse().join('');
+  }
+  decrypt(messageToDecript, key) {
+      if (!messageToDecript || !key) {
+          throw new Error('Incorrect arguments!');
+      }
+
+      const keyArr = key.toUpperCase().repeat(Math.ceil(messageToDecript.length / key.length)).split('');
+      let k = 0;
+
+      const result = messageToDecript.toUpperCase().split('').map((x, i) => {
+          if (x.charCodeAt() >= 65 && x.charCodeAt() <= 90) {
+              let letter = x.charCodeAt() - 65;
+              const delta = keyArr[i - k].charCodeAt() - 65
+              let letterprom = (letter - delta) < 0 ? letter - delta + 26 : letter - delta;
+              
+              letter = letterprom + 65;
+              return String.fromCharCode(letter);
+          } else {
+              k++
+              return x;
+          }
+      });
+      return this.isDirect ? result.join('') : result.reverse().join('');
   }
 }
 
