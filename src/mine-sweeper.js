2cba0869a0bbaca369f2d77dcc275c45d71fabf5
Demetrius81 <shizimaniak@gmail.com>
2023-12-15 11:58:00 +0300

    feat: mine-sweeper is done

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,11 +23,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  function getCount(matrix, i, j) {
+    const down = i - 1 < 0 ? 0 : matrix[i - 1][j] ? 1 : 0;
+    const downLeft = i - 1 < 0 || j - 1 < 0 ? 0 : matrix[i - 1][j - 1] ? 1 : 0;
+    const left = j - 1 < 0 ? 0 : matrix[i][j - 1] ? 1 : 0;
+    const upLeft =
+      i + 1 >= matrix.length || j - 1 < 0 ? 0 : matrix[i + 1][j - 1] ? 1 : 0;
+    const up = i + 1 >= matrix.length ? 0 : matrix[i + 1][j] ? 1 : 0;
+    const upRight =
+      i + 1 >= matrix.length || j + 1 >= matrix[i].length < 0
+        ? 0
+        : matrix[i + 1][j + 1]
+        ? 1
+        : 0;
+    const right = j + 1 >= matrix[i].length ? 0 : matrix[i][j + 1] ? 1 : 0;
+    const downRight =
+      i - 1 < 0 || j + 1 >= matrix[i].length < 0
+        ? 0
+        : matrix[i - 1][j + 1]
+        ? 1
+        : 0;
+
+    return up + down + left + right + downLeft + downRight + upLeft + upRight;
+  }
+
+  const result = Array();
+
+  for (let i = 0; i < matrix.length; i++) {
+    result.push(new Array(matrix[i].length).fill(0));
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j]) {
+        result[i][j] = 1;
+      } else {
+        result[i][j] = getCount(matrix, i, j);
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
